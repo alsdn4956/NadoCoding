6a2777a321decfb2133c249da2d7019941cc7ba7
MinwooJang <46252059+alsdn4956@users.noreply.github.com>
2023-07-02 23:17:42 +0900

    Update ch9.py

--- ch9.py
+++ ch9.py
@@ -1,86 +1,3 @@
-#클래스** 
-'''  
-name = "보병" #이름
-hp = 40 #체력
-damage = 5 #공격력
-
-print("{} 유닛을 생성했습니다.".format(name))
-print("체력 {}, 공격력 {}\n".format(hp,damage))
-
-
-tank_name = "탱크" #이름
-tank_hp = 150 #체력
-tank_damage = 35 #공격력
-
-print("{} 유닛을 생성했습니다.".format(tank_name))
-print("체력 {}, 공격력 {}\n".format(tank_hp,tank_damage))
-
-
-
-    
-tank2_name = "탱크" #이름
-tank2_hp = 150 #체력
-tank2_damage = 35 #공격력
-
-print("{} 유닛을 생성했습니다.".format(tank2_name))
-print("체력 {}, 공격력 {}\n".format(tank2_hp,tank2_damage))
-
-
-
-
-
-# 공격함수 / 공격 부분은 두 유닛이 공통으로 사용. 함수로 정의
-
-def attack(name, location, damage):
-    print("{} : {} 방향 적군을 공격합니다. [공격력{}]".format(name,location,damage))
-        
-attack(name, "1시", damage)
-attack(tank_name, "1시", tank_damage)
-attack(tank2_name, "1시", tank2_damage)
-
-
-#클래스와 객체 생성하기
-
-# 형식 : class 클래스명 :
-    #def 메서드명(self, 전달값1, 전달값2 ..):
-    
-#형식 : self.변수명= 값
-
-class Unit:
-    def __init__(self, name, hp, damage):
-        self.name = name #인스턴트 변수 name에 전달값 name 저장
-        self.hp = hp #인스턴트 변수 hp에 전달값 hp 저장
-        self.damage = damage #인스턴트 변수 damage에 전달값 damage 저장
-        
-        print("{} 유닛을 생성했습니다.".format(name))
-        print("체력 {}, 공격력 {}".format(hp,damage))
-        
-# 형식 : 객체명 = 클래스명(전달값1, 전달값2 ...)
-
-soldier1= Unit("보병",40,5)
-soldier2= Unit("보병",40,5)
-soldier3= Unit("탱크",150,35)
-
-#__init__ 은 생성자라고 한다.
-
-# 전투기 : 공중유닛, 은폐 불가
-stealth1 = Unit("전투기", 80,5)
-#인스턴스 변수 접근
-print("유닛 이름 : {}, 공격력 : {}".format(stealth1.name, stealth1.damage))
-
-
-stealth2= Unit("업그레이드한 전투기", 80,5)
-
-stealth2.cloaking= True
-
-if stealth2.cloaking == True:
-    print("{}는 현재 은폐 상태입니다.".format(stealth2.name))
-    
-#오류발생
-#if stealth1.cloaking== True :
- #   print("{}는 현재 은폐 상태입니다.".format(stealth1.name))
-'''
-
 
 
 #일반 유닛
@@ -117,7 +34,8 @@ class AttackUnit(Unit) :
         print("{} : {} 방향 적군을 공격합니다. [공격력 {}]".format(self.name,location,self.damage))
     
 
-class Soldier(AtaackUnit):
+#보병 유닛
+class Soldier(AttackUnit):
     def __init__(self):
         AttackUnit.__init__(self,"보병",40,5,1)
         
@@ -128,47 +46,86 @@ class Soldier(AtaackUnit):
             print("{} : 강화제를 사용합니다. (HP 10 감소)".format(self.name))
         else:
             print("{} : 체력이 부족해 기술을 사용할 수 없습니다.".format(self.name))
+
             
+            
+#탱크 유닛
 class Tank(AttackUnit):
     #시지 모드 : 탱크를 지상에 고정, 이동불가, 공격력 증가
-    siege_developed = Flase #시지모드 개발여부, 클래스 변수로 정의
+    siege_developed = False #시즈모드 개발여부, 클래스 변수로 정의
  
     
     def __init__(self):
-        AttackUnit.__init__init(self,"탱크",150,35,1)
-        self.seige_mode= False #시지모드 (해제상태), 인스턴스 변수로 정의
-                               
-    
-        
-        
+        AttackUnit.__init__(self,"탱크",150,35,1)
+        self.seige_mode= False #시즈모드 (해제상태), 인스턴스 변수로 정의
+       
+        #시즈 모드 설정
+    def set_siege_mode(self):
+        if Tank.siege_developed == True :   #시즈모드가 개발되지 않았으면 바로 전환
+            return
+        #현재 일반 모드일 때
+        if self.siege_mode == False:
+            print("{} : 시즈 모드로 전환합니다.".format(self.name))
+            self.damage *= 2
+            self.siege_mode = True
+            
+        #현재 시즈 모드일 때
+        else :
+            print("{} : 시즈 모드를 해제합니다.".format(self.name_))
+            self.damage //=2
+            self.siege_mode= False
+             
+  
+
+
+#비행 기능        
 class Flyable:
     def __init__(self,flying_speed): # 비행속도
         self.flying_speed = flying_speed
         
-        
+       
     def fly(self, name, location):
         print("{} : {} 방향으로 날아갑니다. [속도 {}]".format(name,location,self.flying_speed))
         
         
         
 # 공중 공격 유닛
-
-class FlyableAttackUnit(AttackUnit, Flyable) :
+class FlyableAttackUnit(AttackUnit, Flyable) :  #다중상속
     #유닛 이름, 체력, 공격력, 비행속도
     def __init__(self, name, hp, damage, flying_speed):
         AttackUnit.__init__(self,name,hp,damage,0) #지상 이동 속도 0
         Flyable.__init__(self,flying_speed)
         
     def move(self,location):
-        print("[공중 유닛 이동]")
         self.fly(self.name, location)
         
-    
 
-    
+        
+        
+# 전투기 유닛
+class Stealth(FlyableAttackUnit):
+    def __init__(self):
+        FlyableAttackUnit.__init__(self, "전투기", 80,20,5)
+        self.cloaked = False
+        
+        
+        
+    def cloaking(self):
+        #현재 은폐 모드일 때
+        if self.cloaked == True:
+            print("{} : 은폐 모드를 해제합니다.".format(self.name))
+            self.cloaked = False
+            
+        #현재 은폐 모드가 아닐 때
+        else:
+            print("{} : 은폐 모드를 설정합니다.".format(self.name))
+            self.cloaked = True
+            
+
+
    
 #건물 유닛
-class BuildingUnit(Unit):
+'''class BuildingUnit(Unit):
     def __init__(self, name, hp, location):
         #Unit.__init__(self, name, hp, 0) #건물 못 움직이므로 location = 0
         super().__init__(name,hp,0)  #부모클래스 접근, self 없이 사용
@@ -178,53 +135,117 @@ class BuildingUnit(Unit):
         
     
 supply_depot = BuildingUnit("보급고", 500,"7시")  #위에 건물유닛 배정받음 따라서  name, hp, location 
+'''
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        
-        
-        
-# 요격기
 #interceptor = FlyableAttackUnit("요격기",200,6,5)
 #interceptor.fly(interceptor.name, "3시")
         
 
     
 # 호버 바이크 : 지상유닛, 기동성 좋음
-hoverbike = AttackUnit("호버 바이크", 80 ,20,10) #지상 이동 속도 10
+#hoverbike = AttackUnit("호버 바이크", 80 ,20,10) #지상 이동 속도 10
 
 #우주 순양함 : 공중유닛, 체력 굉장히 좋음, 공격력도 좋음
-spacecruiser = FlyableAttackUnit("우주 순양함", 500,25,3) #비행속도 3
+#spacecruiser = FlyableAttackUnit("우주 순양함", 500,25,3) #비행속도 3
 
-hoverbike.move("11시")
-spacecruiser.move("9시")
-
-
+#hoverbike.move("11시")
+#spacecruiser.move("9시")
 
 
+def game_start():
+    print("[알림] 새로운 게임을 시작합니다.")
+    
+    
+def game_over():
+    print("Player : Good Game")
+    print("[Player] 님이 게임에서 퇴장했습니다.")
+    
+
+    
+    
+    
+#게임 시작
+game_start()
+
+#보병 3기 생성
+so1 = Soldier()
+so2 = Soldier()
+so3 = Soldier()
+
+#탱크 2기 생성
+ta1 = Tank()
+ta2 = Tank()
+
+#전투기 1기 생성
+st1 = Stealth()          #함수를 만들고 시행을 할 때는 변수 설정 후 st1= 실행하고 싶은 함수()
+
+
+
+#유닛 일괄 관리(생성된 모든 유닛 추가)
+attack_units = []
+attack_units.append(so1)
+attack_units.append(so2)
+attack_units.append(so3)
+attack_units.append(ta1)
+attack_units.append(ta2)
+attack_units.append(st1)
+
+#전군 이동
+for unit in attack_units:
+    unit.move("1시")
+
+    
+Tank.siege_developed = True
+print("[알림] 탱크의 시즈 모드 개발이 완료됐습니다.")
+    
+    
+
+
+#공격 준비와 전군 공격
+# 형식 : isinstance(객체명, 클래스명)
+
+
+#공격 모드 준비 : (보병: 강화제, 탱크: 시즈모드, 전투기: 은폐 모드)
+for unit in attack_units:
+    if isinstance(unit, Soldier): #Soldier 클래스의 인스턴스이면 강화제
+        unit.booster()
+        
+    elif isinstance(unit, Tank):  #Tank 클래스의 인스턴스이면 시즈모드
+        unit.set_siege_mode()
+        
+    elif isinstance(unit, Stealth) :#Stealth 클래스의 인스턴스이면 은폐모드
+        unit.cloaking()
+        
+        
+
+        
+
+from random import *        
+        
+#전군 공격
+for unit in attack_units:
+    unit.attack("1시")
+        
+#전군 피해
+for unit in attack_units:
+    unit.damaged(randint(5,20))  #피해는 5~20 무작위로 받음
+    
+game_over()
+
+
+
+
+
+
+
+
+
+
+    
+    
 
 
 
